1c69ceda78a77a35973f262b1235c37713100a7d
Rolnik Boris <rollnicker@mail.ru>
2024-02-06 23:41:58 +0300

    Первая версия Не реализовано ветвление conftest

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ import pydantic
 class Settings(pydantic.BaseModel):
     dotenv.load_dotenv()
 
-    bstack_userName: str = os.getenv('bstack_userName')
-    bstack_accessKey: str = os.getenv('bstack_accessKey')
-    remote_url: str = os.getenv('remote_url')
+    bstack_userName: str = os.getenv('USERNAME')
+    bstack_accessKey: str = os.getenv('ACCESKEY')
+    remote_url: str = os.getenv('REMOTE_URL')
     deviceName: str = 'Google Pixel 7'
     platformVersion: str = "13.0"
     app: str = 'bs://sample.app'
